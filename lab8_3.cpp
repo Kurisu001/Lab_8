01cfacf484ba9230ebb9a4f547722e380e62ac3d
Kittipong <anonymous51124@gamil.com>
2024-12-06 15:43:06 +0700

    lab8_3 complete

--- lab8_3.cpp
+++ lab8_3.cpp
@@ -1,3 +1,37 @@
+#include<iostream>
+
+using namespace std;
+
+int main(){
+    int age,hight,bou;
+    string soul;
+    cout << "Enter your age: ";
+    cin >> age;
+
+    if (age <= 25){
+        cout << "Enter your height: ";
+        cin >> hight;
+        if(hight < 100) soul = "Chopper";
+        else if(hight < 180) soul = "Usopp";
+        else {
+            cout << "Enter your bounty: ";
+            cin >> bou;
+            soul = (bou > 1.1e9) ? "Zoro" : "Sanji";
+        }
+
+    }else if(age <= 60){
+        cout << "Enter your bounty: ";
+        cin >> bou;
+        soul = (bou > 5e8) ? "Jinbe" : "Franky";
+    }
+    else soul = "Brook";
+
+    cout << "Your character = " << soul;
+
+    return 0;
+}
+
+
 /*
 "Enter your age: "
 "Enter your height: "
